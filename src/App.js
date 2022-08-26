aa5cfbb46834e9f3f0b866df7d81766c97008470
ABHISHEK TIWARI (Frontend-Developer) <tiwariabhishek27611@gmail.com>
2022-08-26 11:08:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="arrow-right"></div>
+      <div className="half-circle"></div>
+      <div className="quarter-circle-top-left"></div>
+      <div className="quarter-circle-top-right"></div>
+      <div className="quarter-circle-bottom-left"></div>
+      <div className="quarter-circle-bottom-right"></div>
+    </div>
+  );
+}
